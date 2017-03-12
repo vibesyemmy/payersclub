434f20ab34d44a197db8ae935549b8161eef8682
Thompson Edolo <verygreenboi@live.com>
2017-03-12 12:44:30 +0100

    Add logout to authed header

--- src/client/js/layout/header.component.js
+++ src/client/js/layout/header.component.js
@@ -6,6 +6,7 @@ class AppHeaderCtrl {
 
     this.appName = AppConstants.appName;
     this.currentUser = User.current;
+    this.logout = User.logout.bind(User);
 
     $scope.$watch('User.current', (newUser)=> {
       this.currentUser = newUser;
