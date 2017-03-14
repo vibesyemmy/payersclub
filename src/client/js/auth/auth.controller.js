178b5d2d42d0f99d267b14e4c76eaacb08184a85
Thompson Edolo <verygreenboi@live.com>
2017-03-14 22:34:38 +0100

    To lowercase

--- src/client/js/auth/auth.controller.js
+++ src/client/js/auth/auth.controller.js
@@ -13,6 +13,7 @@ class AuthCtrl {
 
   submitForm() {
     this.isSubmitting = true;
+    this.formData.username = this.formData.username.toLowerCase();
     this._User.attemptAuth(this.authType, this.formData, this.plan).then(
       (res) => {
         this._$state.go('app.home');
